48727fac8b73ea3a67d4014015f4a8b01d5336c4
gzim921 <gzimiseni7@gmail.com>
2020-03-16 15:16:05 +0100

    Changes

--- spec/luhn_spec.rb
+++ spec/luhn_spec.rb
@@ -1,7 +1,5 @@
 RSpec.describe Tools::Luhn do
-  let(:luhn) { Tools::Luhn.new('3') }
-
-  it "doesn\'t allow single digit" do
+  it 'doesn\'t allow single digit strings' do
     luhn = Tools::Luhn.new('3')
     expect(luhn.valid?).to be false
   end
@@ -16,13 +14,68 @@ RSpec.describe Tools::Luhn do
     expect(luhn.valid?).to be false
   end
 
-  it 'remains valid if digits reserved' do
+  it 'remains valid if digits reversed' do
     luhn = Tools::Luhn.new('059')
-    expect(luhn.invalid?).to be true
+    expect(luhn.valid?).to be true
   end
 
   it 'becomes invalid if digits reversed' do
     luhn = Tools::Luhn.new('59')
+    expect(luhn.valid?).to be true
+  end
+
+  it 'allows a valid canadian sin' do
+    luhn = Tools::Luhn.new('055 444 285')
+    expect(luhn.valid?).to be true
+  end
+
+  it 'doesn\'t allow invalid canadian sin' do
+    luhn = Tools::Luhn.new('055 444 286')
+    expect(luhn.valid?).to be false
+  end
+
+  it 'doesn\'t allow invalid credit card' do
+    luhn = Tools::Luhn.new('8273 1232 7352 0569')
+    expect(luhn.valid?).to be false
+  end
+
+  it 'doesn\'t allow valid strings with a non digit included' do
+    luhn = Tools::Luhn.new('055a 444 285')
+    expect(luhn.valid?).to be false
+  end
+
+  it 'doesn\'t allow valid strings with a non digit added at the end' do
+    luhn = Tools::Luhn.new('059a')
+    expect(luhn.valid?).to be false
+  end
+
+  it 'doesn\'t allow valid strings with punctuation included' do
+    luhn = Tools::Luhn.new('055-444-285')
+    expect(luhn.valid?).to be false
+  end
+
+  it 'doesn\'t allow valid strings with symbols included' do
+    luhn = Tools::Luhn.new('05£ 44$ 285')
+    expect(luhn.valid?).to be false
+  end
+
+  it 'doesn\'t allow single zero with space' do
+    luhn = Tools::Luhn.new(' 0')
+    expect(luhn.valid?).to be false
+  end
+
+  it 'allows more digits than a single zero'do
+    luhn = Tools::Luhn.new('000 0')
+    expect(luhn.valid?).to be true
+  end
+
+  it 'says input digit 9 is correctly converted to output digit 9' do
+    luhn = Tools::Luhn.new('091 253 765')
+    expect(luhn.valid?).to be true
+  end
+
+  it 'doesn\'t allow strings with non digits' do
+    luhn = Tools::Luhn.new(':986 534')
     expect(luhn.valid?).to be false
   end
 end
